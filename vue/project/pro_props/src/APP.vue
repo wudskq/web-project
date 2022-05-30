fc60a55b000c1b08000c4999a4a3d130a560c6a9
chenfangchao <socrates11@foxmail.com>
2022-05-31 00:10:46 +0800

    vue_props

---
+++ vue/project/pro_props/src/APP.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <Student name="张三" :age=18 sex="男"/> 
+    <hr>
+     <Student name="张三" :age="18" /> 
+</div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+export default {
+    name:'App',
+    components:{
+        Student
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/project/pro_props/src/components/Student.vue
@@ -0,0 +1,45 @@
+<template>
+        <div class="school">
+            <h1>标题:{{msg}}</h1>
+            <h2>学生姓名:{{name}}</h2>
+            <h2>学生性别:{{sex}}</h2>
+            <h2>学生年龄{{age+1}}</h2>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return{
+            msg:'我正在学习Vue'
+        }
+    },
+    //接收参数(简单接收)
+    // props:['name','sex','age']
+    //限定类型接收
+    // props:{
+    //     name: String,
+    //     sex: String,
+    //     age: Number
+    // }
+    props:{
+        name:{
+            type:String,
+            required: true //代表该参数为必要的
+        },
+        sex:{
+            type:String,
+            required: false //代表该参数为必要的
+        },
+        age:{
+            type:Number,
+            required: false //代表该参数为必要的
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
