0b1d10af2a9e57835a74eea400b0b9f0d1184af1
chenfangchao <socrates11@foxmail.com>
2022-05-30 23:46:03 +0800

    vue-ref

---
+++ vue/project/pro_ref/src/APP.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <!--传统方式 -->
+    <h1 v-text="msg" id="id_1"></h1>
+     <button ref="btn" @click="showDom">点击输出上方dom元素</button>
+    <!-- vue方式 -->
+    <h1 v-text="msg1" ref="id_2"></h1>
+    <button @click="showDom1">点击输出上方dom元素</button>
+    <school ref="sch"/>
+</div>
+</template>
+
+<script>
+import School from './components/School.vue'
+export default {
+    name:'App',
+    data(){
+        return{
+            msg:'欢迎学习Vue',
+            msg1:'欢迎学习Vue1'
+        }
+    },
+    components:{
+        School
+    },
+    methods: {
+        showDom(){
+            const data = document.querySelector('#id_1');
+            console.log(data);
+        },
+        showDom1(){
+            console.log(this.$refs.id_2);
+            console.log(this.$refs.btn);
+            console.log(this.$refs.sch);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/project/pro_ref/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="school">
+<h1>{{name}}</h1>
+<h1>{{address}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data(){
+        return{
+        name:'二中',
+        address:'中国'
+        }
+    }
+}
+</script>
+
+<style>
+.school{
+    background-color: pink;
+}
+</style>
