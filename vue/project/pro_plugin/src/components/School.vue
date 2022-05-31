a044873ec52c51680f45ce12ac8ef3bed2673044
chenfangchao <socrates11@foxmail.com>
2022-05-31 11:27:21 +0800

    vue插件

---
+++ vue/project/pro_plugin/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+        <div class="school">
+            <h1>标题:{{msg}}</h1>
+            <h2 >学校名称:{{name}}</h2>
+            <h2>学校地址:{{address}}</h2>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return{
+            msg:'我正在学习Vue',
+            name:'学校',
+            address:'中国'
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/project/pro_plugin/src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+        <div class="school">
+            <h1>标题:{{msg | filte}}</h1>
+            <h2 >学生姓名:{{name}}</h2>
+            <h2>学生性别:{{sex}}</h2>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return{
+            msg:'我正在学习Vue',
+            name:'张三',
+            sex:'男'
+        }
+    },
+
+}
+    
+</script>
+
+<style>
+
+</style>
