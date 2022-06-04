57bfd87f6dfac4571033c98870ecb31adcb07955
chenfangchao <socrates11@foxmail.com>
2022-06-05 04:00:26 +0800

    getter

---
+++ vue/vuex/pro_getters/src/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+     <div>
+         选择要加的数
+         <select v-model.number="value">
+             <option value="1">1</option>
+             <option value="2">2</option>
+             <option value="3">3</option>
+         </select>
+         当前值为: {{$store.state.data}}
+         放大10倍后的值为: {{$store.getters.bigSum}}
+         <button @click="Incremental">点击加一</button>
+         <button @click="Decrement">点击减一</button>
+         <button @click="Oddnumber">点击当前数为奇数时加</button>
+         <button @click="Waitadd">等待一段时间后增加</button>
+     </div>
+</template>
+
+<script>
+export default {
+    name:'Count',
+    data(){
+        return{
+            value:1
+        }
+    },
+    methods: {
+        Incremental(){
+            this.$store.dispatch('incre',this.value);
+        },
+        Decrement(){
+             this.$store.dispatch('decr',this.value);
+        },
+        Oddnumber(){
+            if(this.$store.state.data %2 !== 0){
+              this.$store.dispatch('odd',this.value);  
+            }
+        },
+        Waitadd(){
+            setInterval(()=>{
+                 this.$store.dispatch('wait',this.value);
+            },2000);
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vuex/pro_getters/src/store/index.js
@@ -0,0 +1,62 @@
+//创建核心的vuex中的store
+import Vuex from 'vuex'
+import Vue from 'vue'
+//安装VueX插件
+Vue.use(Vuex);
+//用于响应组件中的动作
+const actions = {};
+//用于加工数据
+const mutations = {};
+//用于存储共享数据
+const state = {
+    //数据的和
+    data: 1,
+};
+//用于将state中的数据进行加工
+const getters = {
+    bigSum(state) {
+        return state.data * 10
+    }
+};
+//创建store对象并暴露
+export default new Vuex.Store({
+    actions: {
+        incre(context, value) {
+            // console.log('actions被调用了');
+            context.commit('Incre', value);
+        },
+        decr(context, value) {
+            // console.log('actions被调用了');
+            context.commit('Decr', value);
+        },
+        odd(context, value) {
+            // console.log('actions被调用了');
+            context.commit('Odd', value);
+        },
+        wait(context, value) {
+            // console.log('actions被调用了');
+            context.commit('Wait', value);
+        },
+    },
+    mutations: {
+        Incre(state, value) {
+            // console.log('mutations被调用了', context, value);
+            state.data = state.data + value;
+        },
+        Decr(state, value) {
+            // console.log('mutations被调用了', context, value);
+            state.data = state.data - value;
+        },
+        Odd(state, value) {
+            // console.log('mutations被调用了', context, value);
+            state.data = state.data + value;
+        },
+        Wait(state, value) {
+            // console.log('mutations被调用了', context, value);
+            state.data = state.data + value;
+        },
+
+    },
+    state,
+    getters
+});
