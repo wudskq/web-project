d64764f4d0730dfdba2b39213a055486830394d4
chenfangchao <socrates11@foxmail.com>
2022-06-07 02:40:37 +0800

    路由模式

---
+++ vue/router/pro6/src/router/index.js
@@ -0,0 +1,30 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+//引入组件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import Message from '../pages/Message'
+import News from '../pages/News'
+
+//创建并暴露一个路由器
+export default new VueRouter({
+    mode: 'history',
+    routes: [{
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/home',
+            component: Home,
+            children: [{
+                    path: 'message',
+                    component: Message
+                },
+                {
+                    path: 'news',
+                    component: News
+                }
+            ]
+        }
+    ]
+})
