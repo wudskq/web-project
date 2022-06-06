db5e4356d4ca4c53de51b4ab8fe3092adc0fcb08
chenfangchao <socrates11@foxmail.com>
2022-06-06 16:42:41 +0800

    编程式路由导航

---
+++ vue/router/pro4/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+	<h2>我是Home的内容</h2>
+	<router-link :to="{name:'News'}" active-class="active">news</router-link>
+	&nbsp;
+	<!-- 开启历史记录replace模式 -->
+	<router-link to="/home/message" active-class="active">message</router-link>
+	<router-view></router-view>
+	</div> 
+</template>
+
+<script>
+	export default {
+		name:'Home',
+	}
+</script>
