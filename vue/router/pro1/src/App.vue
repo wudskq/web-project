7b5214433366c6da6a438214d0c1673391ef9e43
chenfangchao <socrates11@foxmail.com>
2022-06-06 10:38:01 +0800

    路由的注意点

--- vue/router/pro1/src/App.vue
+++ vue/router/pro1/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>Vue Router Demo</h2></div>
-      </div>
+      <!-- 一般组件 -->
+      <Banner/>
     </div>
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
@@ -11,8 +10,8 @@
 					<!-- 原始html中我们使用a标签实现页面的跳转 -->
           <!-- <a class="list-group-item active" href="./about.html">About</a> -->
           <!-- <a class="list-group-item" href="./home.html">Home</a> -->
-
 					<!-- Vue中借助router-link标签实现路由的切换 -->
+          <!-- 路由组件 -->
 					<router-link class="list-group-item" active-class="active" to="/about">About</router-link>
           <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
         </div>
@@ -30,7 +29,11 @@
 </template>
 
 <script>
+import Banner from './components/Banner.vue'
 	export default {
 		name:'App',
+    components:{
+      Banner
+    }
 	}
 </script>
---
+++ vue/router/pro1/src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<h2>我是About的内容</h2>
+</template>
+
+<script>
+	export default {
+		name:'About',
+		beforeDestroy() {
+			console.log('About组件即将被销毁');
+		},
+		mounted() {
+        console.log('About组件挂在完毕',this);
+        },
+	}
+</script>
